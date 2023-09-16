ed7ae6069ba15c1677462c0fe241f4f2a85c0c76
fouad1233 <fouadiadhami@gmail.com>
2023-09-16 17:28:34 +0300

    pose_subscriber rewrited objected oritently

--- catkin_ws/src/my_robot_controller/scripts/pose_subscriber.py
+++ catkin_ws/src/my_robot_controller/scripts/pose_subscriber.py
@@ -2,17 +2,17 @@
 import rospy
 from turtlesim.msg import Pose
 
-def pose_callback(msg:Pose):
-    rospy.loginfo("("+str(msg.x)+", "+str(msg.y)+")")
+def poseCallback(pose_message: Pose):
+    rospy.loginfo(f"({pose_message.x}, {pose_message.y}, {pose_message.theta})")
     
     
-
-if __name__ == '__main__':
+def main():
+    rospy.init_node('turtle_pose_subscriber') # Initiate a Node named 'pose_subscriber'
+    sub = rospy.Subscriber('/turtle1/pose', Pose, callback=poseCallback) # Create a Subscriber object that will listen to the /turtle1/pose topic and will call the poseCallback() function each time it reads something from the topic
+    rate = rospy.Rate(2) # Set a publish rate of 2 Hz
     
-    rospy.init_node('turtle_pose_subscriber')
-    #make a rospy subscriber object
-    sub = rospy.Subscriber('/turtle1/pose', Pose, callback= pose_callback)
+    rospy.spin() # Create a loop that will keep the program in execution
     
-    rospy.loginfo("Node has been started")
-    #spin() simply keeps python from exiting until this node is stopped and callbacks will continue to be called
-    rospy.spin()
+    
+if __name__ == "__main__":
+    main()
