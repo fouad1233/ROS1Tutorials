5141730f56f99277d4b29dc18a9aa9565bf2195e
fouad1233 <fouadiadhami@gmail.com>
2023-09-16 20:54:13 +0300

    turtle_controller.py rewrited objected orientedly

--- catkin_ws/src/my_robot_controller/scripts/turtle_controller.py
+++ catkin_ws/src/my_robot_controller/scripts/turtle_controller.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
+
 import rospy
-from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
 
+class RobotController:
+    def __init__(self):
+        rospy.init_node('turtle_controller')
+        self.pub = rospy.Publisher('/turtle1/cmd_vel',  Twist, queue_size=10) 
+        self.sub = rospy.Subscriber('/turtle1/pose', Pose, callback=self.poseCallback)
 
-def pose_callback(pose: Pose):
-    cmd = Twist()
-    if pose.x > 9.0 or pose.x < 2.0 or pose.y > 9.0 or pose.y < 2:
-        cmd.linear.x = 1.0
-        cmd.angular.z = 0.7
-    else:
-        cmd.linear.x = 5.0
-        cmd.angular.z = 0.0
-    pub.publish(cmd)
-if __name__ == '__main__':
-    rospy.init_node('turtle_controller')
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    
-    sub = rospy.Subscriber('/turtle1/pose', Pose, callback=pose_callback)
-    rospy.loginfo('Node turtle_controller started')
-    
+    def poseCallback(self,pose: Pose):
+        cmd = Twist()
+        cmd.linear.x,cmd.angular.z = pose.linear_velocity, pose.angular_velocity
+        rospy.loginfo(f"{cmd.linear.x}, {cmd.angular.z}")
+        if pose.x > 9.0 or pose.x < 1.0 or pose.y > 9.0 or pose.y < 1.0:
+            cmd.linear.x = 1.0
+            cmd.angular.z = 1.4
+        else:   
+            cmd.linear.x = 5.0
+            cmd.angular.z = 0.0
+        self.pub.publish(cmd)
+        
+def main():
+    node = RobotController()
+    rospy.loginfo("robot_controller node started") 
     rospy.spin()
+
+if __name__ == "__main__":
+    main()
